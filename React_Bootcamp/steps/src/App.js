8d7306fea05433c85794389763f81c3644919869
4hund3rbird <kshirsagaraniket10@gmail.com>
2023-10-19 14:19:54 +0530

    steps - start

---
+++ React_Bootcamp/steps/src/App.js
@@ -0,0 +1,12 @@
+export default function App() {
+  return (
+    <div className="steps">
+      <div className="nubers">
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+        <div>4</div>
+      </div>
+    </div>
+  );
+}
