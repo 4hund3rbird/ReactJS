e50fbfdfba1a20ee882e9059b884b305abc362b3
4hund3rbird <kshirsagaraniket10@gmail.com>
2023-10-21 18:04:00 +0530

    covered the initial project

--- React_Bootcamp/far-away-list/src/App.js
+++ React_Bootcamp/far-away-list/src/App.js
@@ -1,15 +1,31 @@
 import { useState } from "react";
 
 export default function App() {
+  let itemcount = 4;
   let [list, updateList] = useState([
-    { id: 1, item: "soap", qty: 3, packed: true },
+    { id: 1, item: "soap", qty: 3, packed: false },
     { id: 2, item: "socks", qty: 4, packed: false },
     { id: 3, item: "cola", qty: 9, packed: false },
     { id: 4, item: "brush", qty: 2, packed: false },
   ]);
   function removeitem(id) {
     updateList((list) => {
+      itemcount--;
       return list.filter((item) => item.id !== id);
+    });
+  }
+  function additem(i, q) {
+    updateList((list) => {
+      itemcount++;
+      return [
+        ...list,
+        {
+          id: Date.now(),
+          item: i,
+          qty: q,
+          packed: false,
+        },
+      ];
     });
   }
   function packitem(id) {
@@ -17,17 +33,15 @@ export default function App() {
       return list.map((item) => {
         if (item.id === id) {
           item.packed = !item.packed;
-          return item;
-        } else {
-          return item;
         }
+        return item;
       });
     });
   }
   return (
     <div className="app">
       <Logo />
-      <Form />
+      <Form fun3={additem} />
       <Packinglist l={list} fun1={removeitem} fun2={packitem} />
       <Stats />
     </div>
@@ -37,19 +51,32 @@ export default function App() {
 function Logo() {
   return <h1>🌴 Far Away List ✈️</h1>;
 }
-function Form() {
+function Form({ fun3 }) {
+  const [description, setdescription] = useState("");
+  const [qty, setqty] = useState(1);
+
+  function handlesubmit(e) {
+    e.preventDefault();
+    console.log(e);
+    fun3(description, qty);
+  }
   return (
-    <form className="add-form">
+    <form className="add-form" onSubmit={handlesubmit}>
       <p>What do you need for your trip 😍.</p>
-      <select>
+      <select value={qty} onChange={(e) => setqty(parseInt(e.target.value))}>
         {Array.from({ length: 20 }, (e, i) => i + 1).map((e) => (
           <option key={e} value={e}>
             {e}
           </option>
         ))}
       </select>
-      <input type="text"></input>
-      <button>Add</button>
+      <input
+        type="text"
+        value={description}
+        onChange={(e) => setdescription(e.target.value)}
+        placeholder="item..."
+      ></input>
+      <button onClick={handlesubmit}>Add</button>
     </form>
   );
 }
@@ -75,17 +102,20 @@ function Packinglist({ l, fun1, fun2 }) {
   );
 }
 function Item({ id, i, q, ispacked, handleupdate, handlepack }) {
+  const [ischecked, updatecheck] = useState(false);
   return (
     <li>
       <span>
         <input
           type="checkbox"
+          checked={ischecked}
           onChange={(evt) => {
+            updatecheck(!ischecked);
             handlepack(id);
           }}
         ></input>
       </span>
-      <span className={ispacked ? "packeditem" : " "}>
+      <span className={ischecked ? "packeditem" : " "}>
         {i} - {q}
       </span>
       <span>
