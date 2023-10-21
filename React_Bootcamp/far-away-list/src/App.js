b8cb467f6494e5cef00e993cb21866b8375509cc
4hund3rbird <kshirsagaraniket10@gmail.com>
2023-10-21 14:11:12 +0530

    updated the farawaylist

--- React_Bootcamp/far-away-list/src/App.js
+++ React_Bootcamp/far-away-list/src/App.js
@@ -2,14 +2,33 @@ import { useState } from "react";
 
 export default function App() {
   let [list, updateList] = useState([
-    { item: "soap", qty: 3 },
-    { item: "Brush", qty: 1 },
+    { id: 1, item: "soap", qty: 3, packed: true },
+    { id: 2, item: "socks", qty: 4, packed: false },
+    { id: 3, item: "cola", qty: 9, packed: false },
+    { id: 4, item: "brush", qty: 2, packed: false },
   ]);
+  function removeitem(id) {
+    updateList((list) => {
+      return list.filter((item) => item.id !== id);
+    });
+  }
+  function packitem(id) {
+    updateList((list) => {
+      return list.map((item) => {
+        if (item.id === id) {
+          item.packed = !item.packed;
+          return item;
+        } else {
+          return item;
+        }
+      });
+    });
+  }
   return (
     <div className="app">
       <Logo />
       <Form />
-      <Packinglist l={list} />
+      <Packinglist l={list} fun1={removeitem} fun2={packitem} />
       <Stats />
     </div>
   );
@@ -22,24 +41,62 @@ function Form() {
   return (
     <form className="add-form">
       <p>What do you need for your trip 😍.</p>
+      <select>
+        {Array.from({ length: 20 }, (e, i) => i + 1).map((e) => (
+          <option key={e} value={e}>
+            {e}
+          </option>
+        ))}
+      </select>
       <input type="text"></input>
       <button>Add</button>
     </form>
   );
 }
-function Packinglist({ l }) {
+function Packinglist({ l, fun1, fun2 }) {
   return (
-    <ul className="list">
-      {l.map((e) => {
-        return <Item i={e.item} q={e.qty} />;
-      })}
-    </ul>
+    <div className="list">
+      <ul>
+        {l.map((e) => {
+          return (
+            <Item
+              key={e.id}
+              id={e.id}
+              i={e.item}
+              q={e.qty}
+              ispacked={e.packed}
+              handleupdate={fun1}
+              handlepack={fun2}
+            />
+          );
+        })}
+      </ul>
+    </div>
   );
 }
-function Item({ i, q }) {
+function Item({ id, i, q, ispacked, handleupdate, handlepack }) {
   return (
     <li>
-      {i} - {q} <input type="checkbox"></input>
+      <span>
+        <input
+          type="checkbox"
+          onChange={(evt) => {
+            handlepack(id);
+          }}
+        ></input>
+      </span>
+      <span className={ispacked ? "packeditem" : " "}>
+        {i} - {q}
+      </span>
+      <span>
+        <button
+          onClick={() => {
+            handleupdate(id);
+          }}
+        >
+          ❌
+        </button>
+      </span>
     </li>
   );
 }
