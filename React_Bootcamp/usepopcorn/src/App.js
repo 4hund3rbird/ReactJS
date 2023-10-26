8ef46d844fc6a4af2ec2b93c3086df9da7b828b0
4hund3rbird <kshirsagaraniket10@gmail.com>
2023-10-26 17:37:28 +0530

    created the starrating component

--- React_Bootcamp/usepopcorn/src/App.js
+++ React_Bootcamp/usepopcorn/src/App.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-
+import Starrating from "./Starrating";
 const tempMovieData = [
   {
     imdbID: "tt1375666",
@@ -186,49 +186,5 @@ function Watchedsummary({ watched }) {
         </p>
       </div>
     </div>
-  );
-}
-
-function Starrating() {
-  const [rating, setrating] = useState(0);
-  let onClick = false;
-  return (
-    <div className="rating">
-      <span>
-        {Array.from({ length: 10 }, (e, i) => (
-          <span
-            key={i}
-            onClick={() => (onClick = true)}
-            onMouseEnter={(evt) => {
-              console.log(evt);
-              setrating(i + 1);
-            }}
-            onMouseLeave={() => !onClick && setrating(0)}
-          >
-            <Stars
-              fill={i + 1 > rating ? "none" : "yellow"}
-              stroke={"yellow"}
-            />
-          </span>
-        ))}
-      </span>
-      <span>
-        <p>{rating}</p>
-      </span>
-    </div>
-  );
-}
-
-function Stars({ fill, stroke }) {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 20 20"
-      fill={fill}
-      stroke={stroke}
-      width="20px"
-    >
-      <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-    </svg>
   );
 }
