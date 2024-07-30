920824d5a4932ff21e026d67b14416f362effb05
4hund3rbird <kshirsagaraniket10@gmail.com>
2024-07-31 00:46:04 +0530

    cors

--- verce-api/api/userdata.js
+++ verce-api/api/userdata.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-
+const port = 3000;
 const userdata = require("../data/Userdata.json");
 
 app.use(cors());
+
 app.get("/api/userdata", (req, res) => {
   res.json(userdata);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
 
 module.exports = app;
